8cd4f58502be6584a5a961e49a8c72faa8212ac0
Prasadikengalle <prasadikengalle@gmail.com>
2024-02-09 19:30:13 +0530

    list all products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,66 @@
-import { Link} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Navbar, Nav , Container, Row, Col, Form , Button , Card} from "react-bootstrap"
 import NavigationBar from './NavigationBar';
+import axios from "axios";
 
 
 const Home = () => {
+
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        getProducts();
+    },[]);
+
+    const getProducts = async () =>{
+        const response = await axios.get("http://localhost:8081/products");
+        setProducts(response.data);
+    };
+
+    const ProductCard = ({ product }) => {
+        
+        return (
+            <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>
+                        ID: {product.id}
+                        
+                    </Card.Text>
+                    <Button variant="primary" >
+                        View Details
+                    </Button>
+                </Card.Body>
+            </Card>
+        );
+    };
+
 
     return (
         <>
             <NavigationBar/>
 
-            <h1>Home</h1>
+            <Container className="mt-5">
+                
 
-            <ul>
-                <li>
-                    <Link to="/products">Products</Link>
+                <Row>
+                
+                    <Col md={8}>
+                        <Row>
+                        <h1 className="mb-4">Products</h1>
+                            {products && products.map((product) => (
+                                <ProductCard key={product.id} product={product} />
+                            ))}
+                        </Row>
+                    </Col>
 
-
-                </li>
-
-                <button className="btn btn-primary">Load Products</button>
-            </ul>
-
-            
-
-            
+                    <Col md={4}>
+                        <h2 className="mb-4 text-center">Add New Product</h2>
+                        
+                    </Col>
+                </Row>
+            </Container>
 
         </>
 
